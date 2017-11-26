a47d4d8fd95c042c62a022913ff352a2f3a67aaf
tuoli <702574512@qq.com>
2017-11-26 14:19:05 +0800

    完成进度条功能
    完成进度条和时间显示功能

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+$(function(){
 var play = document.getElementById("play");//获取播放按钮
 var pause = document.getElementById("pause");//获取暂停按钮
 var stop = document.getElementById("stop");//获取停止按钮
@@ -18,3 +19,50 @@ stop.onclick = function(){
 range.oninput = function(){
     myAudio.volume = range.value * 0.01;    //调节音量
 }
+    
+
+AudioControl('myAudio')
+function AudioControl(id){
+  // 音频控制进度条
+  var audio = document.getElementById(id);
+  $(audio).on('loadedmetadata',function(){
+ audio.pause();
+ // 进度条控制
+  $(document).on('touchend','.progress-back',function(e){
+    var x = e.originalEvent.changedTouches[0].clientX-this.offsetLeft;
+    var X = x < 0 ? 0 : x ;
+    var W = $(this).width();
+    var place = X > W ? W : X;
+    audio.currentTime = (place/W).toFixed(2)*audio.duration
+    $(this).children().css({width:(place/W).toFixed(2)*100+"%"})
+  });
+
+
+  })
+  setInterval(function () {
+    var currentTime = audio.currentTime;
+    setTimeShow(currentTime);
+  }, 1000);
+  // 设置播放时间
+function setTimeShow(t) {
+  t = Math.floor(t);
+  var playTime = secondToMin(audio.currentTime);
+  $(".time").html(playTime);
+  $('.time-show').text(secondToMin(audio.duration));
+  $('.progress-back').css({width:(t/audio.duration).toFixed(4)*100+"%"})
+}
+  // 计算时间
+function secondToMin(s) {
+  var MM = Math.floor(s / 60);
+  var SS = s % 60;
+  if (MM < 10)
+    MM = "0" + MM;
+  if (SS < 10)
+    SS = "0" + SS;
+  var min = MM + ":" + SS;
+  return min.split('.')[0];
+}
+}
+
+
+})
